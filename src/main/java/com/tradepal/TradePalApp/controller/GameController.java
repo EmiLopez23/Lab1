4f833652d8bc6778cbc41b16fb0d8f5310f71a0a
EmiLopez23 <emiliano.lopez@ing.austral.edu.ar>
2023-04-14 21:14:36 -0300

    game controller y service added

--- src/main/java/com/tradepal/TradePalApp/controller/GameController.java
+++ src/main/java/com/tradepal/TradePalApp/controller/GameController.java
@@ -4,9 +4,7 @@ import com.tradepal.TradePalApp.repository.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin("http://localhost:3000")
@@ -15,10 +13,7 @@ public class GameController {
     @Autowired
     GameRepository gameRepository;
 
-
-
-
-    @RequestMapping("/all")
+    @GetMapping("/all")
     public ResponseEntity<?> findAllGames(){
         return new ResponseEntity<>(gameRepository.findAll(), HttpStatus.OK);
     }
---
+++ src/main/java/com/tradepal/TradePalApp/services/GameService.java
@@ -0,0 +1,9 @@
+package com.tradepal.TradePalApp.services;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GameService {
+
+}
