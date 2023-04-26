376b385b25e1be0a88fc9d882b608cca923b5a58
Benjamin Vivanco <benjamin.vivanco@ing.austral.edu.ar>
2023-04-26 01:34:18 -0300

    VALUE != NAME

--- src/main/java/com/tradepal/TradePalApp/repository/CategoryValueRepository.java
+++ src/main/java/com/tradepal/TradePalApp/repository/CategoryValueRepository.java
@@ -2,9 +2,10 @@ package com.tradepal.TradePalApp.repository;
 
 import com.tradepal.TradePalApp.model.CategoryValue;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface CategoryValueRepository extends JpaRepository<CategoryValue,Long> {
-    List<CategoryValue> findAllByNameIn(List<String> names);
+    List<CategoryValue> findAllByValueIn(List<String> values);
 }
