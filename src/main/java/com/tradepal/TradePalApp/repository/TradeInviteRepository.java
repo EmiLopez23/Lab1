828cd4971fb0d6335012cb13e82e657f3c1d9971
Benjamin Vivanco <benjamin.vivanco@ing.austral.edu.ar>
2023-06-18 17:13:36 -0300

    Query Fix

--- src/main/java/com/tradepal/TradePalApp/repository/TradeInviteRepository.java
+++ src/main/java/com/tradepal/TradePalApp/repository/TradeInviteRepository.java
@@ -9,9 +9,9 @@ import java.util.List;
 
 public interface TradeInviteRepository extends JpaRepository<TradeInvite, Long> {
 
-    @Query("SELECT ti FROM TradeInvite ti WHERE (ti.requester.id = :userId AND ti.status = 'ACCEPTED' AND ti.post.active=false ) OR ti.post.id IN (SELECT p.id FROM Post p WHERE p.user.id = :userId AND p.active = true)")
+    @Query("SELECT ti FROM TradeInvite ti WHERE (ti.requester.id = :userId AND ti.status = com.tradepal.TradePalApp.model.TradeStatus.ACCEPTED AND ti.post.active=false ) OR ti.post.id IN (SELECT p.id FROM Post p WHERE p.user.id = :userId AND p.active = true)")
     List<TradeInvite> getTradeInviteNotifications(@Param("userId") Long userId);
 
-    @Query("SELECT ti FROM TradeInvite ti WHERE ti.status <> 'SENT' AND (ti.requester.id = :userId OR  ti.post.id IN (SELECT p.id FROM Post p WHERE p.user.id = :userId ))")
+    @Query("SELECT ti FROM TradeInvite ti WHERE ti.status <> com.tradepal.TradePalApp.model.TradeStatus.SENT AND (ti.requester.id = :userId OR  ti.post.id IN (SELECT p.id FROM Post p WHERE p.user.id = :userId ))")
     List<TradeInvite> getUserConfirmedTradeInvites(@Param("userId") Long userId);
 }
